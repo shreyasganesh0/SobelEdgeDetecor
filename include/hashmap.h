29502bbdb402a421b89708aa8606989bcbb7c824
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-03-18 23:46:23 -0400

    Create hashmap functions

--- include/hashmap.h
+++ include/hashmap.h
@@ -1,1 +1,15 @@
 #ifndef HASHMAP_H
+#define HASHMAP_H
+
+typedef struct {
+    u8 fill; 
+    int val;
+} map_t;
+
+typedef struct {
+    map_t *map;
+    int capcity;
+    int size;
+} hashmap_t;
+
+
--- src/hashmap.c
+++ src/hashmap.c
@@ -13,6 +13,48 @@ u64 hash_function(const char *data) {
     return hash;
 }
 
-void hashmap_create(
+hashmap_t *hashmap_create(int capacity) {
 
+    hashmap_t *hashmap = malloc(sizeof(hashmap_t));
+    hashmap->map = malloc(capacity * sizeof(map_t));
 
+    hashmap->size = 0;
+    hashmap->capacity = capacity;
+    for (int i = 0; i < capacity; i++) {
+
+        hashmap->map[idx].val = 0;
+        hashmap->map[idx].fill = 0;
+    }
+
+    return hashmap;
+}
+
+void hashmap_insert(const char *byte, hashmap_t *hashmap, u8 value) {
+
+    u64 hash = hash_function(byte);
+
+    int idx = hash % hashmap->capacity;
+
+    while (hashmap->map[idx].fill == 1) {
+        idx++;
+        idx = idx - (idx >= hashmap->capacity) * hashmap->capacity;
+    }
+    hashmap->map[idx].val = value;
+    hashmap->map[idx].fill = 1;
+
+    return;
+}
+
+int hashmap_index_get(const char *key, hashmap_t *hashmap) {
+
+    u64 hash = hash_function(key);
+
+    int idx = hash % hashmap->capacity;
+
+    while (hashmap->map[idx].fill == 1) {
+        idx++;
+        idx = idx - (idx >= hashmap->capacity) * hashmap->capacity;
+    }
+
+    return idx;
+}
